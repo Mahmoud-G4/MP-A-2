367099ae4a8220a2da8056aa37e59e67d9cc3e3b
Mahmoud-G4 <120424125+Mahmoud-G4@users.noreply.github.com>
2024-05-04 00:33:48 +0300

    database Update

--- MP-Assignment-2/app/src/main/java/com/example/mpassignment2/MainActivity.kt
+++ MP-Assignment-2/app/src/main/java/com/example/mpassignment2/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mpassignment2.ui.theme.MPAssignment2Theme
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //  the database
+        val database = Firebase.database
         setContent {
             MPAssignment2Theme {
                 // A surface container using the 'background' color from the theme
